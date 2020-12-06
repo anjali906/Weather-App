b6e1eb84cc133f0638214fe3ad062d03565d8e14
anjali906 <anjalireddy136@gmail.com>
2020-12-06 23:39:24 +0530

    changes to display

--- src/app/forecast.service.ts
+++ src/app/forecast.service.ts
@@ -10,9 +10,9 @@ export class ForecastService {
   constructor(private http: HttpClient) { }
 
   info(city: string){
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=767c4138efe1353bc76c2d428a4fc0f0')
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=767c4138efe1353bc76c2d428a4fc0f0`)
     .pipe( tap((res) =>{
-      console.log('ddddd',res);
+      console.log('ddddd',city);
     }))
     
   }
